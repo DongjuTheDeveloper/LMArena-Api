2bbb51fae5fb8a22e0c7ad2cd77fe8a3113ce0ca
김동주 <judong1094@gmail.com>
2025-08-20 23:05:49 +0900

    Update starter.py

--- starter.py
+++ starter.py
@@ -8,12 +8,16 @@ script_dir = Path(__file__).parent
 os.chdir(script_dir)
 
 browser_exe = script_dir / "browser" / "ungoogled-chromium-portable.exe"
-if browser_exe.exists():
-    first_run_file = script_dir / "browser" / "data" / "First Run"
-    
-    if not first_run_file.exists():
-        print("Ready For First Run..")
 
+proxy_script = script_dir / "server" / "proxy_server.py"
+if proxy_script.exists():
+    # subprocess.Popen([sys.executable, str(proxy_script)], 
+    #             creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+    subprocess.Popen([sys.executable, str(proxy_script)])
+
+
+if browser_exe.exists():
     print("Ready For Main Run..")
     
     subprocess.Popen([str(browser_exe), "https://lmarena.ai"], 
@@ -27,12 +31,3 @@ if browser_exe.exists():
 else:
     print(f"Error: {browser_exe} file not found")
     sys.exit(1)
-
-time.sleep(1)
-
-proxy_script = script_dir / "server" / "proxy_server.py"
-if proxy_script.exists():
-    # subprocess.Popen([sys.executable, str(proxy_script)], 
-    #             creationflags=subprocess.CREATE_NEW_CONSOLE)
-
-    subprocess.Popen([sys.executable, str(proxy_script)])
